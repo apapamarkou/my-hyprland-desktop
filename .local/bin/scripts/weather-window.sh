c3d49dcf7b74e8b74ee001958d14135bb0b89e09
Andrianos Papamarkou <papamarkoua@gmail.com>
2024-10-05 18:38:23 +0300

    add scripts bin directory

---
+++ .local/bin/scripts/weather-window.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+echo
+echo
+echo $1
+curl 'https://wttr.in/thessaloniki?3pq&lang=el' 
+read -n 1
+
---
+++ .local/bin/scripts/weather.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Σύντομη πρόβλεψη για εμφάνιση στο Waybar
+short_weather="   $(curl -s "wttr.in/?format=1&lang=el")   "
+
+# Πλήρης πρόβλεψη για το tooltip (Θεσσαλονίκη)
+full_weather="  $(curl "https://wttr.in/thessaloniki?format=4&lang=el")  "
+
+# Εμφάνιση της σύντομης πρόβλεψης και της πλήρους ως tooltip
+echo -e "$short_weather\n$full_weather"
+
